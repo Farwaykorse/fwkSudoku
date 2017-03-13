213af562cab1a6cd89d6b83fafc29862de020222
Roelf-Jilling Wolthuis <r-j.wolthuis@live.com>
2017-03-13 23:30:43 +0100

    Add more testObjects

--- UnitTest/stdafx.h
+++ UnitTest/stdafx.h
@@ -8,8 +8,11 @@
 #include "targetver.h"
 
 // Headers for CppUnitTest
-#include "CppUnitTest.h"
+#include <CppUnitTest.h>
 
 // TODO: reference additional headers your program requires here
 #include <stdexcept>
 #include <type_traits>
+
+// Files to pre-compile
+#include "TestObjects.h"
