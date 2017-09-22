63599a3ca8386deb0243e584d1005e4428c82bc3
Roelf-Jilling <r-j.wolthuis@live.com>
2017-09-23 00:51:35 +0200

    re-solve: initial file structure

---
+++ Sudoku/Solver.fwd.h
@@ -0,0 +1,12 @@
+﻿//===--	Sudoku/Solver.fwd.h												--===//
+//
+//	Forward declaration header for Solver.h
+//===---------------------------------------------------------------------===//
+
+namespace Sudoku
+{
+template<int N>
+class Solver;
+
+} // namespace Sudoku
+
