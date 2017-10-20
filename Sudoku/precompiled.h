80749d58ffed08252a08cbe0943c062e3bc97dab
Roelf-Jilling <r-j.wolthuis@live.com>
2017-10-20 11:39:02 +0200

    Sudoku: Add GSL to precompiled

--- Sudoku/precompiled.h
+++ Sudoku/precompiled.h
@@ -1,6 +1,5 @@
-﻿// -----------------------------------------------------------------------------
-// precompiled.h : header for Precompiled Header
-// -----------------------------------------------------------------------------
+﻿//===--	Sudoku/precompiled.h											--===//
+//
 // - Compiled from precompiled.cpp
 // - Included in project by compiler configuration
 //		Clang:	-include "precompiled.h"
@@ -9,15 +8,14 @@
 // file for standard system include files,
 // or project specific include files that are used frequently, but
 // are changed infrequently
-//
-//////////////////////////
+//===---------------------------------------------------------------------===//
 #pragma once
 
 #include <array>
 #include <bitset>
 #include <vector>
-
 #include <algorithm>
+#include <gsl/gsl>
 #include <iterator>
 #include <limits>
 #include <memory>
