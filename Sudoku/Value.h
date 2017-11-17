d233329e91fcd11c955824a1aab54a5578986785
Roelf-Jilling <r-j.wolthuis@live.com>
2017-11-17 17:26:56 +0100

    Add value_t, first implementation

---
+++ Sudoku/Value.h
@@ -0,0 +1,12 @@
+﻿//===--	Sudoku/Value.h													--===//
+//
+//	Value type
+//===---------------------------------------------------------------------===//
+#pragma once
+
+namespace Sudoku
+{
+using value_t = unsigned int;
+// allows use as a bitmask
+
+} // namespace Sudoku
