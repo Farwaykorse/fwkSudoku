22329b88ebb35e0750f0a9014d93af3645ae6e38
Roelf-Jilling <r-j.wolthuis@live.com>
2018-04-23 09:33:13 +0200

    +traits for new Board_Section implementation

---
+++ Sudoku/Board_Section_traits.h
@@ -0,0 +1,77 @@
+﻿//====---- Sudoku/Board_Section_traits.h                              ----====//
+//
+// Type traits for Board_Section classes.
+//====--------------------------------------------------------------------====//
+#pragma once
+
+#include <type_traits>
+
+// Forward declarations
+#include "Board.fwd.h"
+
+
+namespace Sudoku::Board_Sections
+{
+inline namespace traits
+{
+	using namespace ::Sudoku::Board_Sections;
+
+	template<typename T>
+	struct is_Section : std::false_type {};
+	template<typename T, int N, Section S, bool C>
+	struct is_Section<Board_Section2<T, N, S, C>> : std::true_type {};
+	template<typename T>
+	inline static constexpr bool is_Section_v = is_Section<T>::value;
+
+	template<typename T>
+	struct is_const_Section : std::false_type {};
+	template<typename T, int N, Section S, bool is_const>
+	struct is_const_Section<Board_Section2<T, N, S, is_const>>
+		: std::integral_constant<bool, is_const> {};
+	template<typename T>
+	inline static constexpr bool is_const_Section_v =
+		is_const_Section<T>::value;
+
+	template<typename T>
+	struct is_const_Row : std::false_type {};
+	template<typename T, int N>
+	struct is_const_Row<const_Row<T, N>> : std::true_type {};
+
+	template<typename T>
+	struct is_Row : std::false_type {};
+	template<typename T, int N>
+	struct is_Row<Row<T, N>> : std::true_type {};
+	template<typename T, int N>
+	struct is_Row<const_Row<T, N>> : std::true_type {};
+	template<typename T>
+	inline static constexpr bool is_Row_v = is_Row<T>::value;
+
+	template<typename T>
+	struct is_const_Col : std::false_type {};
+	template<typename T, int N>
+	struct is_const_Col<const_Col<T, N>> : std::true_type {};
+
+	template<typename T>
+	struct is_Col : std::false_type {};
+	template<typename T, int N>
+	struct is_Col<Col<T, N>> : std::true_type {};
+	template<typename T, int N>
+	struct is_Col<const_Col<T, N>> : std::true_type {};
+	template<typename T>
+	inline static constexpr bool is_Col_v = is_Col<T>::value;
+
+	template<typename T>
+	struct is_const_Block : std::false_type {};
+	template<typename T, int N>
+	struct is_const_Block<const_Block<T, N>> : std::true_type {};
+
+	template<typename T>
+	struct is_Block : std::false_type {};
+	template<typename T, int N>
+	struct is_Block<Block<T, N>> : std::true_type {};
+	template<typename T, int N>
+	struct is_Block<const_Block<T, N>> : std::true_type {};
+	template<typename T>
+	inline static constexpr bool is_Block_v = is_Block<T>::value;
+} // namespace traits
+} // namespace Board_Sections
---
+++ SudokuTests/Board_Section_traits.cpp
@@ -0,0 +1,85 @@
+﻿//====---- SudokuTests/Board_Section.cpp                              ----====//
+//
+// Unit tests for the template class Sudoku::Board_Section
+//====--------------------------------------------------------------------====//
+
+// Class under test
+#include <Sudoku/Board_Section_traits.h>
+
+
+namespace SudokuTests
+{
+using namespace ::Sudoku;
+using namespace ::Sudoku::Board_Sections;
+
+using dataT        = int;
+constexpr int size = 3;
+
+using Row         = typename Board_Sections::Row<dataT, size>;
+using Col         = typename Board_Sections::Col<dataT, size>;
+using Block       = typename Board_Sections::Block<dataT, size>;
+using const_Row   = typename Board_Sections::const_Row<dataT, size>;
+using const_Col   = typename Board_Sections::const_Col<dataT, size>;
+using const_Block = typename Board_Sections::const_Block<dataT, size>;
+
+namespace Section_traits
+{
+	// inline namespace traits
+	static_assert(std::is_same_v<
+				  Board_Sections::is_Section<int>,
+				  Board_Sections::traits::is_Section<int>>);
+	using namespace ::Sudoku::Board_Sections::traits;
+
+	static_assert(not is_Section_v<int>);
+	static_assert(is_Section_v<Row>);
+	static_assert(is_Section_v<Col>);
+	static_assert(is_Section_v<Block>);
+	static_assert(is_Section_v<const_Row>);
+	static_assert(is_Section_v<const_Col>);
+	static_assert(is_Section_v<const_Block>);
+
+	static_assert(not is_const_Section<int>::value);
+	static_assert(not is_const_Section_v<int>);
+	static_assert(not is_const_Section_v<Row>);
+	static_assert(not is_const_Section_v<Col>);
+	static_assert(not is_const_Section_v<Block>);
+	static_assert(is_const_Section_v<const_Row>);
+	static_assert(is_const_Section_v<const_Col>);
+	static_assert(is_const_Section_v<const_Block>);
+
+	static_assert(not is_Row<int>::value);
+	static_assert(not is_Row_v<int>);
+	static_assert(is_Row<Row>::value);
+	static_assert(is_Row_v<Row>);
+	static_assert(is_Row_v<const_Row>);
+	static_assert(not is_Row_v<Col>);
+	static_assert(not is_Row_v<const_Col>);
+	static_assert(not is_Row_v<Block>);
+	static_assert(not is_Row_v<const_Block>);
+
+	static_assert(not is_Col<int>::value);
+	static_assert(not is_Col_v<int>);
+	static_assert(is_Col<Col>::value);
+	static_assert(is_Col_v<Col>);
+	static_assert(is_Col_v<const_Col>);
+	static_assert(not is_Col_v<Row>);
+	static_assert(not is_Col_v<const_Row>);
+	static_assert(not is_Col_v<Block>);
+	static_assert(not is_Col_v<const_Block>);
+
+	static_assert(not is_Block<int>::value);
+	static_assert(not is_Block_v<int>);
+	static_assert(is_Block<Block>::value);
+	static_assert(is_Block_v<Block>);
+	static_assert(is_Block_v<const_Block>);
+	static_assert(not is_Block_v<Row>);
+	static_assert(not is_Block_v<const_Row>);
+	static_assert(not is_Block_v<Col>);
+	static_assert(not is_Block_v<const_Col>);
+
+	// For references to Section:
+	static_assert(not is_Section_v<Row&>); //? Should this work?
+} // namespace traits
+
+
+} // namespace SudokuTests
