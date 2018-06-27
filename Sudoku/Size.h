0a5aa172649737471d1cdaf7f2e5d07671d7c8f7
Roelf-Jilling <r-j.wolthuis@live.com>
2018-06-27 23:20:04 +0200

    ~extend checks on Size

--- Sudoku/Size.h
+++ Sudoku/Size.h
@@ -1,7 +1,7 @@
-﻿//===--- Sudoku/Size.h                                                  ---===//
+﻿//====---- Sudoku/Size.h                                              ----====//
 //
 // The size definition that rules them all.
-//===----------------------------------------------------------------------===//
+//====--------------------------------------------------------------------====//
 #pragma once
 
 #include <limits>
@@ -17,14 +17,16 @@ struct Size
 
 	static_assert(
 		base < elem && base < full && elem < full,
-		"class Location: Board size out of bounds");
+		"struct Size: Board size out of bounds.");
 	static_assert(
 		N < std::numeric_limits<int>::max() / base &&
 			N < std::numeric_limits<int>::max() / elem &&
-			N < std::numeric_limits<int>::max() / elem / base,
-		"struct Size: Board size to big for size system integer");
+			N < std::numeric_limits<int>::max() / elem / base &&
+			N < std::numeric_limits<int>::max() / full &&
+			N < std::numeric_limits<int>::max() / full / base, // count_options
+		"struct Size: Board size too large for system integer size.");
+	// fails at N > 35 when int less than 32-bits.
 };
-
 
 template<int N>
 constexpr int base_size = Size<N>::base; // default 3
@@ -32,6 +34,5 @@ template<int N>
 constexpr int elem_size = Size<N>::elem; // default 9
 template<int N>
 constexpr int full_size = Size<N>::full; // default 81
-
 
 } // namespace Sudoku
