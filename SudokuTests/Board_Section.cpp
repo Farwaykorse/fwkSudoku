fa5008273e1b0b42c03f6bd4d3273e2a36c90a0f
Roelf-Jilling <r-j.wolthuis@live.com>
2019-12-17 19:15:12 +0100

    ! MSVC warning C4834: discarding return value of function with 'nodiscard' attribute
    VS2019 v16.4

--- SudokuTests/Board_Section.cpp
+++ SudokuTests/Board_Section.cpp
@@ -187,15 +187,17 @@ namespace constructors
 	{
 		Board board{};
 
-		EXPECT_DEBUG_DEATH((Row(board, -1)), "is_valid_size");
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Row A(board, -1), "is_valid_size");
 		[[maybe_unused]] Row row0(board, 0);
 		[[maybe_unused]] Row row8(board, 8);
-		EXPECT_DEBUG_DEATH((Row(board, 9)), "is_valid_size");
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Row A(board, 9), "is_valid_size");
 
-		EXPECT_DEBUG_DEATH((const_Row(board, -1)), "is_valid_size");
-		[[maybe_unused]] const_Row crow0(board, 0);
-		[[maybe_unused]] const_Row crow8(board, 8);
-		EXPECT_DEBUG_DEATH((const_Row(board, 9)), "is_valid_size");
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] const_Row A(board, -1), "is_valid_size");
+		[[maybe_unused]] const const_Row crow0(board, 0);
+		[[maybe_unused]] const const_Row crow8(board, 8);
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] const_Row A(board, 9), "is_valid_size");
 	}
 
 	//====----------------------------------------------------------------====//
@@ -211,12 +213,12 @@ namespace constructors
 	{
 		Board board{};
 
-		EXPECT_DEBUG_DEATH((Row(board, Loc{-1})), "is_valid");
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Row A(board, Loc{-1}), "is_valid");
 		[[maybe_unused]] Row row0(board, Loc{0});
 		[[maybe_unused]] Row row1(board, Loc{9});
 		[[maybe_unused]] Row row1_again(board, Loc{10});
 		[[maybe_unused]] Row row8(board, Loc{8, 0});
-		EXPECT_DEBUG_DEATH((Row(board, Loc{81})), "is_valid");
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Row A(board, Loc{81}), "is_valid");
 	}
 	// is_constructible between Board_Section types
 	// To const:
@@ -303,15 +305,17 @@ namespace constructors
 		constexpr int last = elem_size<size> - 1;
 		Row row(board, 2);
 
-		EXPECT_DEBUG_DEATH(Col(row, -1), "is_valid_size"); // in location(elem)
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Col A(row, -1), "is_valid_size");
 		EXPECT_EQ(Col(row, 0).id(), 0);
 		EXPECT_EQ(Col(row, 1).id(), 1);
 		EXPECT_EQ(Col(row, 3).id(), 3);
 		EXPECT_EQ(Col(row, last).id(), last);
-		EXPECT_DEBUG_DEATH(Col(row, elem_size<size>), "is_valid_size");
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] Col A(row, elem_size<size>), "is_valid_size");
 
-		EXPECT_DEBUG_DEATH(Block(row, -1), "is_valid_size"); // location(elem)
-		EXPECT_DEBUG_DEATH(Block(row, elem_size<size>), "is_valid_size");
+		EXPECT_DEBUG_DEATH([[maybe_unused]] Block A(row, -1), "is_valid_size");
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] Block A(row, elem_size<size>), "is_valid_size");
 		if constexpr (size == 2)
 		{
 			EXPECT_EQ(Block(row, 0).id(), 2);
@@ -360,16 +364,21 @@ namespace constructors
 		Board board{};
 		constexpr int last = ::Sudoku::elem_size<size> - 1;
 
-		EXPECT_DEBUG_DEATH(const_Col(Row(board, 1), -1), "is_valid_size");
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] const_Col A(Row(board, 1), -1), "is_valid_size");
 		EXPECT_EQ(const_Col(Row(board, 1), 0).id(), 0);
 		EXPECT_EQ(const_Col(Row(board, 1), 1).id(), 1);
 		EXPECT_EQ(const_Col(Row(board, 1), 3).id(), 3);
 		EXPECT_EQ(const_Col(Row(board, 1), last).id(), last);
-		EXPECT_DEBUG_DEATH(const_Col(Row(board, 1), last + 1), "is_valid_size");
-
-		EXPECT_DEBUG_DEATH(const_Block(Row(board, 1), -1), "is_valid_size");
 		EXPECT_DEBUG_DEATH(
-			const_Block(Row(board, 1), last + 1), "is_valid_size");
+			[[maybe_unused]] const_Col A(Row(board, 1), last + 1),
+			"is_valid_size");
+
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] const_Block A(Row(board, 1), -1), "is_valid_size");
+		EXPECT_DEBUG_DEATH(
+			[[maybe_unused]] const_Block A(Row(board, 1), last + 1),
+			"is_valid_size");
 		if constexpr (size == 2)
 		{
 			EXPECT_EQ(const_Block(Row(board, 1), 0).id(), 2);
