Farwaykorse/fwkSudoku

commit f0baacfc7ade947bdc54a8bc464923879b90ec09

! update excessive checks for changes in MSVC

SudokuTests/Board_Section.cpp
@@ -105,11 +105,11 @@ namespace type_properties
 	static_assert(not std::is_const_v<constT>);
 	static_assert(not std::is_volatile_v<typeT>);
 	static_assert(not std::is_trivial_v<typeT>);
-#if defined(__clang__) || defined(__GNUC__)
-	static_assert(std::is_trivially_copyable_v<typeT>);
-#else
+#if defined(_MSC_VER) && _MSC_VER < 1927
 	static_assert(not std::is_trivially_copyable_v<typeT>);
-#endif // __clang__
+#else
+	static_assert(std::is_trivially_copyable_v<typeT>);
+#endif // Old MSVC
 	static_assert(not std::is_standard_layout_v<typeT>);
 #if not(defined(__ICL) && __ICL <= 1900) &&                                    \
 	not(defined(__APPLE__) && defined(__clang__) &&                            \

SudokuTests/Location.cpp
@@ -43,18 +43,16 @@ namespace compiletime
 	static_assert(not std::is_const_v<typeT>);
 	static_assert(not std::is_volatile_v<typeT>);
 	static_assert(not std::is_trivial_v<typeT>);
-#if defined(__clang__) || defined(__GNUC__)
+#if defined(_MSC_VER) && _MSC_VER < 1927
+	static_assert(not std::is_trivially_copyable_v<typeT>);
+#else
 	static_assert(std::is_trivially_copyable_v<typeT>);
-#if not(defined(__clang__) && __clang_major__ < 6) &&                          \
-	not(defined(__APPLE__) && defined(__clang__) && __clang_major__ < 10)
+#if !(defined(__clang__) && __clang_major__ < 6) &&                            \
+	!(defined(__APPLE__) && defined(__clang__) && __clang_major__ < 10) &&     \
+	!(defined(__ICL) && __ICL <= 1900)
 	static_assert(std::has_unique_object_representations_v<typeT>);
 #endif
-#else
-	static_assert(not std::is_trivially_copyable_v<typeT>);
-#if not(defined(__ICL) && __ICL <= 1900)
-	static_assert(not std::has_unique_object_representations_v<typeT>);
-#endif // __ICL
-#endif // __clang__
+#endif // older compilers
 	static_assert(std::is_standard_layout_v<typeT>);
 	static_assert(not std::is_empty_v<typeT>); // nothing virtual
 	static_assert(not std::is_polymorphic_v<typeT>);
@@ -156,7 +154,7 @@ namespace Location_Block_compiletime
 	static_assert(not std::is_const_v<typeT>);
 	static_assert(not std::is_volatile_v<typeT>);
 	static_assert(not std::is_trivial_v<typeT>);
-#if defined(__GNUC__) && !defined(__clang__)
+#if !defined(__clang__) && !(defined(_MSC_VER) && _MSC_VER < 1927)
 	static_assert(std::is_trivially_copyable_v<typeT>);
 	static_assert(std::has_unique_object_representations_v<typeT>);
 #else

commit a8d96594fa2a68c68cdc336896480452957fcb1d

! fix conditional for Clang on Windows
issue introduced in f0baacfc7ade947bdc54a8bc464923879b90ec09

SudokuTests/Board_Section.cpp
@@ -105,7 +105,7 @@ namespace type_properties
 	static_assert(not std::is_const_v<constT>);
 	static_assert(not std::is_volatile_v<typeT>);
 	static_assert(not std::is_trivial_v<typeT>);
-#if defined(_MSC_VER) && _MSC_VER < 1927
+#if defined(_MSC_VER) && _MSC_VER < 1927 && !defined(__clang__)
 	static_assert(not std::is_trivially_copyable_v<typeT>);
 #else
 	static_assert(std::is_trivially_copyable_v<typeT>);

SudokuTests/Location.cpp
@@ -43,7 +43,7 @@ namespace compiletime
 	static_assert(not std::is_const_v<typeT>);
 	static_assert(not std::is_volatile_v<typeT>);
 	static_assert(not std::is_trivial_v<typeT>);
-#if defined(_MSC_VER) && _MSC_VER < 1927
+#if defined(_MSC_VER) && _MSC_VER < 1927 && !defined(__clang__)
 	static_assert(not std::is_trivially_copyable_v<typeT>);
 #else
 	static_assert(std::is_trivially_copyable_v<typeT>);